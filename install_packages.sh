a521450d9a39fd8872d4929ce1939b61061f5523
zawiskrz <112825491+zawiskrz@users.noreply.github.com>
2025-08-29 09:04:34 +0200

    Update install_packages.sh

--- install_packages.sh
+++ install_packages.sh
@@ -3,16 +3,15 @@ sudo apt update
 sudo apt install \
 mc htop \
 xorg lightdm lightdm-gtk-greeter \
-openbox obconf obmenu obkey \
-lxappearance xdg-utils python3-xdg \
+openbox obconf lxappearance xdg-utils python3-xdg \
 upower dmz-cursor-theme \
 compton conky-all \
-rxvt-unicode-256color tmux gksu \
+rxvt-unicode tmux pkexec \
 tint2 spacefm udevil \
-qt4-qtconfig \
+geany geany-plugin-spellcheck \
+qt5ct \
 gtk2-engines gtk2-engines-murrine gtk2-engines-pixbuf murrine-themes libgtk2.0-bin \
-gtk3-engines-unico \
-gnome-icon-theme gnome-icon-theme-symbolic gnome-themes-standard at-spi2-core \
-mate-icon-theme-faenza mate-themes \
+gnome-icon-theme gnome-icon-theme-symbolic gnome-themes-extra at-spi2-core \
+mate-themes papirus-icon-theme \
 dconf-editor dconf-cli \
-thunar xfce4-terminal network-manager-gnome feh 
+jgmenu
