2b9554282e8d6fabde3128d675b02a97e3bec626
zawiskrz <112825491+zawiskrz@users.noreply.github.com>
2025-08-29 08:57:22 +0200

    Create install_packages.sh

---
+++ install_packages.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+sudo apt update
+sudo apt install \
+mc htop \
+xorg lightdm lightdm-gtk-greeter \
+openbox obconf obmenu obkey \
+lxappearance xdg-utils python3-xdg \
+upower dmz-cursor-theme \
+compton conky-all \
+rxvt-unicode-256color tmux gksu \
+tint2 spacefm udevil \
+qt4-qtconfig \
+gtk2-engines gtk2-engines-murrine gtk2-engines-pixbuf murrine-themes libgtk2.0-bin \
+gtk3-engines-unico \
+gnome-icon-theme gnome-icon-theme-symbolic gnome-themes-standard at-spi2-core \
+mate-icon-theme-faenza mate-themes \
+dconf-editor dconf-cli \
+thunar xfce4-terminal network-manager-gnome feh 
