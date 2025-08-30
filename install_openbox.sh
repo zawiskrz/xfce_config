52ce9a363b647020b9ba943ac89b156fd1531ed8
Krzysztof Zawistowski <krzysztof.zawistowski@gmail.com>
2025-08-30 12:43:15 +0200

    kolejna modyfikacja pliku install_packages.sh

--- install_openbox.sh
+++ install_openbox.sh
@@ -5,19 +5,21 @@ LOGFILE="install_log.txt"
 echo "🔧 Aktualizacja pakietów..." | tee -a "$LOGFILE"
 sudo apt update 2>&1 | tee -a "$LOGFILE"
 
-echo "📦 Instalacja środowiska Openbox..." | tee -a "$LOGFILE"
+echo "📦 Instalacja środowiska Openbox + XFCE Panel..." | tee -a "$LOGFILE"
 sudo apt install -y \
-openssh-server  ufw \
+openssh-server ufw \
 xorg lightdm lightdm-gtk-greeter \
 openbox obconf lxappearance xdg-utils python3-xdg \
-nitrogen jgmenu menu rofi \
+nitrogen rofi \
 upower dmz-cursor-theme \
 compton \
-tint2 papirus-icon-theme \
+papirus-icon-theme \
 network-manager-gnome \
 pulseaudio pulseaudio-utils pulseaudio-module-bluetooth pavucontrol libcanberra-pulse pasystray \
-bluez blueman xfce4-power-manager xfce4-appfinder xfce4-settings \
-thunar thunar-volman gvfs udisks2 xfce4-terminal \
+bluez blueman \
+xfce4-power-manager xfce4-appfinder xfce4-settings xfce4-terminal \
+xfce4-panel xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin xfce4-datetime-plugin xfce4-systemload-plugin \
+thunar thunar-volman gvfs udisks2 \
 firefox-esr thunderbird vlc calibre rhythmbox shotwell \
 libreoffice libreoffice-l10n-pl libreoffice-help-pl \
 wxmaxima \
@@ -25,20 +27,11 @@ python3 python3-pip python3-venv \
 mc htop \
 x11-xserver-utils 2>&1 | tee -a "$LOGFILE"
 
-
 echo "🗂️ Nadpisywanie konfiguracji użytkownika..." | tee -a "$LOGFILE"
 
 # Openbox
 mkdir -p ~/.config/openbox
 cp -f config/openbox/* ~/.config/openbox/
-
-# Tint2
-mkdir -p ~/.config/tint2
-cp -f config/tint2/* ~/.config/tint2/
-
-# JGMenu
-mkdir -p ~/.config/jgmenu
-cp -f config/jgmenu/* ~/.config/jgmenu/
 
 # GTK 3.0
 mkdir -p ~/.config/gtk-3.0
@@ -54,6 +47,8 @@ cp -f tapety/* ~/tapety/
 
 echo "🖼️ Ustawianie tapety pulpitu..." | tee -a "$LOGFILE"
 nitrogen --set-scaled ~/tapety/planety.jpg --save
+
+echo "🛡️ Konfiguracja zapory sieciowej UFW..." | tee -a "$LOGFILE"
 
 # Resetowanie ustawień zapory
 sudo ufw --force reset 2>&1 | tee -a "$LOGFILE"
@@ -80,5 +75,4 @@ echo "✅ Zapora UFW została skonfigurowana i aktywowana." | tee -a "$LOGFILE"
 echo "🔄 Restart LightDM..." | tee -a "$LOGFILE"
 sudo systemctl restart lightdm 2>&1 | tee -a "$LOGFILE"
 
-
-echo "✅ Instalacja zakończona. Środowisko Openbox zostało skonfigurowane." 
+echo "✅ Instalacja zakończona. Środowisko Openbox z panelem XFCE zostało skonfigurowane." | tee -a "$LOGFILE"
