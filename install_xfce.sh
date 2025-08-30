a006aef0b1e2bc2470dfdcd1e0bf8d4934b63b1f
zawiskrz <112825491+zawiskrz@users.noreply.github.com>
2025-08-30 19:16:56 +0200

    Update install_xfce.sh

--- install_xfce.sh
+++ install_xfce.sh
@@ -73,8 +73,11 @@ if [[ "$XFCE" == "true" ]]; then
   sudo localectl set-locale LANG=pl_PL.UTF-8
   sudo localectl set-keymap pl
   sudo localectl set-x11-keymap pl pc105 legacy
+
+  # Ustawienie klawiatury po starcie X11
   echo 'setxkbmap -model pc105 -layout pl -variant legacy' >> ~/.xprofile
   chmod +x ~/.xprofile
+
 
 
   echo "🗂️ Kopiowanie konfiguracji użytkownika..." | tee -a "$LOGFILE"
