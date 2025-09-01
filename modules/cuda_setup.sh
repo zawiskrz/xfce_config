f0a842d8dd6d1a0dbe768ab07831a794eea75264
zawiskrz <zawistowski.krzysztof@gmai.com>
2025-09-01 09:49:56 +0200

    aktulizacja struktury katalogów projektu

--- modules/cuda_setup.sh
+++ modules/cuda_setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 configure_cuda() {
-  echo "⚡ Instalacja CUDA Toolkit..." | tee -a "$LOGFILE"
-  wget "$CUDA_KEYRING_URL" -O cuda-keyring.deb 2>&1 | tee -a "$LOGFILE"
-  sudo dpkg -i cuda-keyring.deb 2>&1 | tee -a "$LOGFILE"
-  sudo apt update
-  sudo apt install -y cuda 2>&1 | tee -a "$LOGFILE"
-  echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc
-  echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+  if lspci | grep -i "nvidia" >/dev/null; then
+    echo "⚡ Instalacja CUDA Toolkit..." | tee -a "$LOGFILE"
+    wget "$CUDA_KEYRING_URL" -O cuda-keyring.deb 2>&1 | tee -a "$LOGFILE"
+    sudo dpkg -i cuda-keyring.deb 2>&1 | tee -a "$LOGFILE"
+    sudo apt update
+    sudo apt install -y cuda 2>&1 | tee -a "$LOGFILE"
+    echo 'export PATH=/usr/local/cuda/bin:$PATH' >> ~/.bashrc
+    echo 'export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH' >> ~/.bashrc
+  else
+    echo "⚠️ Nie wykryto karty NVIDIA. Pomijam instalację CUDA." | tee -a "$LOGFILE"
+  fi
 }
