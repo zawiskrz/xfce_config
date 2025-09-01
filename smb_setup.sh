fa14eb3f0254d5a6ed9d7cd91ebbfd162eab9873
zawiskrz <zawistowski.krzysztof@gmai.com>
2025-09-01 08:08:49 +0200

    aktualiazacja xfce_setup.sh dodałem gnome-package-updater

--- smb_setup.sh
+++ smb_setup.sh
@@ -11,15 +11,24 @@ configure_smb() {
   echo -e "$SAMBA_PASS\n$SAMBA_PASS" | sudo smbpasswd -a "$SAMBA_USER"
   sudo smbpasswd -e "$SAMBA_USER"
 
-  # Tworzenie katalogów
-  mkdir -p /home/$SAMBA_USER/Obrazy /home/$SAMBA_USER/Wideo
-  chmod 770 /home/$SAMBA_USER/Obrazy /home/$SAMBA_USER/Wideo
-  chown $SAMBA_USER:$SAMBA_USER /home/$SAMBA_USER/Obrazy /home/$SAMBA_USER/Wideo
+#  # Tworzenie katalogów
+#  mkdir -p /home/$SAMBA_USER/Obrazy /home/$SAMBA_USER/Wideo
+#  chmod 770 /home/$SAMBA_USER/Obrazy /home/$SAMBA_USER/Wideo
+#  chown $SAMBA_USER:$SAMBA_USER /home/$SAMBA_USER/Obrazy /home/$SAMBA_USER/Wideo
 
   # Backup i konfiguracja smb.conf
   sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.bak
 
   cat <<EOF | sudo tee -a /etc/samba/smb.conf
+
+[Muzyka]
+   path = /home/$SAMBA_USER/Muzyka
+   valid users = $SAMBA_USER
+   browseable = yes
+   writable = yes
+   create mask = 0770
+   directory mask = 0770
+   guest ok = no
 
 [Obrazy]
    path = /home/$SAMBA_USER/Obrazy
