c0a543646423e6f25f2bdca318a52d63167fd80b
zawiskrz <zawistowski.krzysztof@gmail.com>
2025-09-03 12:16:08 +0200

    aktualizacja aplikacji

--- modules/xfce_setup.sh
+++ modules/xfce_setup.sh
@@ -11,7 +11,7 @@ install_environment_packages() {
     gdebi-core unattended-upgrades apt-listchanges \
     mintstick timeshift redshift redshift-gtk powermgmt-base \
     libxapp1 gir1.2-xapp-1.0 xapps-common python3-xapp gdebi \
-    libimobiledevice-1.0-6 libimobiledevice-utils usbmuxd ifuse \
+    libimobiledevice-1.0-6 libimobiledevice-utils usbmuxd ifuse isc-dhcp-client \
     gvfs gvfs-backends gvfs-fuse nautilus \
     bleachbit p7zip-full file-roller 2>&1 | tee -a "$LOGFILE"
 }
